7f827be3ee84678d2a67bd503b7e3c857ab66e8c
amansaluja017 <Amansaluja017@gmail.com>
2025-01-04 16:06:38 +0530

    add login and logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -128,6 +128,7 @@ const loginUser = asyncHandler(async(req, res) => {
     // make access token and refresh token 
     const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
 
+    // remove password and refresh token from response
     const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
 
     const options = {
@@ -135,7 +136,8 @@ const loginUser = asyncHandler(async(req, res) => {
         secure: true
     }
 
-    return response.status(200).cookie("accessToken", accessToken, options)
+    // return response to frontend server
+    return res.status(200).cookie("accessToken", accessToken, options)
     .cookie("refreshToken", refreshToken, options)
     .json(
         new ApiResponse(
@@ -145,6 +147,30 @@ const loginUser = asyncHandler(async(req, res) => {
             },
             "Logged in successfully"
         )
+    )
+})
+
+const logOutUser = asyncHandler(async(req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined,
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200).clearCookie("accessToken", options).clearCookie("refreshToken", options)
+    .json(
+        new ApiResponse(200, {}, "Logged out successfully")
     )
 })
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {registerUser} from '../controllers/user.controller.js';
+import {loginUser,logOutUser,registerUser} from '../controllers/user.controller.js';
 import {upload} from '../middlewares/multer.middleware.js';
 
 const router = express.Router();
@@ -18,5 +18,12 @@ router.route("/register").post(
     registerUser
 );
 
+
+router.route("/login").post(loginUser)
+
+router.route("/logout").post(
+    verifyJWT,
+    logOutUser
+)
 
 export default router
