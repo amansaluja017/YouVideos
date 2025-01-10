6738ce1f3ccc43d41c7be70581924bc1064fe196
amansaluja017 <Amansaluja017@gmail.com>
2025-01-10 20:54:32 +0530

    feat: add video management functions including fetching, updating, deleting, and toggling publish status; fix description typo

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -8,21 +8,21 @@ import {
   deleteFromCloudinary,
 } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import jwt from "jsonwebtoken";
+
 
 const publishAVideo = asyncHandler(async(req, res) => {
-  const { title, discription } = req.body;
+  const { title, description } = req.body;
 
   if(!title) {
     throw new ApiError(401, "Please provide a title")
   }
 
-  if(!discription) {
-    throw new ApiError(401, "Please provide a discription")
+  if(!description) {
+    throw new ApiError(401, "Please provide a description")
   }
 
-  if(!(title && discription)) {
-    throw new ApiError(401, "Please provide a title and discription")
+  if(!(title && description)) {
+    throw new ApiError(401, "Please provide a title and description")
   }
 
   const videoFilePath = req.files?.videoFile[0]?.path
@@ -51,7 +51,7 @@ const publishAVideo = asyncHandler(async(req, res) => {
   const createVideo = await Video.create(
     {
       title,
-      discription,
+      description,
       thumbnail: thumbnail.url,
       videoFile: videoFile.url
     }
@@ -66,6 +66,114 @@ const publishAVideo = asyncHandler(async(req, res) => {
   return res.status(200).json(new ApiResponse(201, createdVideo, "video uploaded successfully"))
 })
 
+const getVideoById = asyncHandler(async(req, res) => {
+  const { videoId } = req.params
+
+  const video = await Video.findById(videoId)
+
+  if(!video) {
+    throw new ApiError(404, "Video not found")
+  }
+
+  return res.status(200).json(new ApiResponse(200, video, "video fetched successfully"))
+})
+
+const updateVideoDetails = asyncHandler(async(req, res) => {
+  const { videoId } = req.params
+  const { title, description } = req.body
+
+  if(!title) {
+    throw new ApiError(400, "Enter new video title")
+  }
+
+  if(!description) {
+    throw new ApiError(400, "Enter new video description")
+  }
+
+  if(!(title && description)) {
+    throw new ApiError(400, "Enter new video title and description")
+  }
+
+  const videoDetails = await Video.findById(videoId).select("title description")
+
+  if(videoDetails.title === title) {
+    throw new ApiError(400, "Enter new video title")
+  }
+
+  if(videoDetails.description === description) {
+    throw new ApiError(400, "Enter new video description")
+  }
+
+  const thumbnailFilePath = req.file?.path
+  
+  if(!thumbnailFilePath) {
+    throw new ApiError(404, "Thumbnail file not found")
+  }
+
+  const thumbnail = await uploadToCloudinary(thumbnailFilePath)
+
+  if(!thumbnail) {
+    throw new ApiError(404, "Failed to upload thumbnail")
+  }
+
+  const deleteThumbnail = await Video.findById(videoId).select("thumbnail")
+  
+  if(deleteThumbnail && deleteThumbnail.thumbnail) {
+    const videoThumbnail = deleteThumbnail.thumbnail
+    const public_id = videoThumbnail.split('/').slice(-1)[0].split('.')[0]
+    await deleteFromCloudinary(public_id)
+  }
+
+  const video = await Video.findByIdAndUpdate(videoId,
+    {
+      $set: {
+        title,
+        description,
+        thumbnail: thumbnail.url,
+      }
+    },
+    { new: true }
+  )
+
+  return res.status(200).json(new ApiResponse(200, video, "video details updated"))
+})
+
+const deleteVideo = asyncHandler(async(req, res) => {
+  const { videoId } = req.params
+
+  const video = await Video.findById(videoId)
+
+  if(!video) {
+    throw new ApiError(404, "Video not found")
+  }else {
+    await Video.deleteOne(video)
+  }
+
+  return res.status(200).json(new ApiResponse(200, null, "video deleted successfully"))
+})
+
+const togglePublishStatus = asyncHandler(async(req, res) => {
+  const { videoId } = req.params
+
+  const video = await Video.findById(videoId).select("isPublished")
+  const videoStatus = video.isPublished
+  console.log(videoStatus)
+
+  await Video.findByIdAndUpdate(videoId,
+    {
+      $set: {
+        isPublished: !videoStatus
+      }
+    }
+  )
+
+  return res.status(200).json(new ApiResponse(200, video, "video status updated"))
+})
+
 export {
-  publishAVideo
+  publishAVideo,
+  getVideoById,
+  updateVideoDetails,
+  deleteVideo,
+  togglePublishStatus
 }
